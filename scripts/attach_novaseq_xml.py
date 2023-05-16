357c44eadc8e47afad177a5f42dfc7027b7d96f8
kedhammar <alfred.kedhammar@scilifelab.se>
2023-05-16 14:45:30 +0200

    More Black formatting

--- scripts/attach_novaseq_xml.py
+++ scripts/attach_novaseq_xml.py
@@ -15,37 +15,52 @@ DESC = """EPP for attaching RunInfo.xml and RunParameters.xml from NovaSeq run d
 Author: Chuan Wang, Science for Life Laboratory, Stockholm, Sweden
 """
 
+
 def main(lims, args):
-    log=[]
+    log = []
     content = None
     process = Process(lims, id=args.pid)
 
     # Copy Read and index parameter from the step "Load to Flowcell (NovaSeq 6000 v2.0)"
-    UDF_to_copy = ['Read 1 Cycles', 'Read 2 Cycles', 'Index Read 1', 'Index Read 2']
+    UDF_to_copy = ["Read 1 Cycles", "Read 2 Cycles", "Index Read 1", "Index Read 2"]
     for i in UDF_to_copy:
         if process.parent_processes()[0].udf.get(i):
-            process.udf[i]=process.parent_processes()[0].udf[i]
+            process.udf[i] = process.parent_processes()[0].udf[i]
     process.put()
 
     # Fetch Flowcell ID
-    FCID=process.parent_processes()[0].output_containers()[0].name
+    FCID = process.parent_processes()[0].output_containers()[0].name
 
     for outart in process.all_outputs():
-        if outart.type == 'ResultFile' and outart.name == 'Run Info':
+        if outart.type == "ResultFile" and outart.name == "Run Info":
             try:
-                lims.upload_new_file(outart,max(glob.glob('/srv/mfs/NovaSeq_data/*{}/RunInfo.xml'.format(FCID)),key=os.path.getctime))
+                lims.upload_new_file(
+                    outart,
+                    max(
+                        glob.glob("/srv/mfs/NovaSeq_data/*{}/RunInfo.xml".format(FCID)),
+                        key=os.path.getctime,
+                    ),
+                )
             except:
                 raise RuntimeError("No RunInfo.xml Found!")
-        elif outart.type == 'ResultFile' and outart.name == 'Run Parameters':
+        elif outart.type == "ResultFile" and outart.name == "Run Parameters":
             try:
-                lims.upload_new_file(outart,max(glob.glob('/srv/mfs/NovaSeq_data/*{}/RunParameters.xml'.format(FCID)),key=os.path.getctime))
+                lims.upload_new_file(
+                    outart,
+                    max(
+                        glob.glob(
+                            "/srv/mfs/NovaSeq_data/*{}/RunParameters.xml".format(FCID)
+                        ),
+                        key=os.path.getctime,
+                    ),
+                )
             except:
                 raise RuntimeError("No RunParameters.xml Found!")
 
+
 if __name__ == "__main__":
     parser = ArgumentParser(description=DESC)
-    parser.add_argument('--pid',
-                        help='Lims id for current Process')
+    parser.add_argument("--pid", help="Lims id for current Process")
     args = parser.parse_args()
 
     lims = Lims(BASEURI, USERNAME, PASSWORD)
