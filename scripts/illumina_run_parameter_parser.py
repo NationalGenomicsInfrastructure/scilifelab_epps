7147d8a897f299fff899e72c05d09a8b52e882dc
liqun-yao <115505443+liqun-yao@users.noreply.github.com>
2026-01-28 16:02:18 +0100

    to make the change ruff compatible

--- scripts/illumina_run_parameter_parser.py
+++ scripts/illumina_run_parameter_parser.py
@@ -509,8 +509,10 @@ def lims_for_NovaSeqXPlus(process, run_dir):
     reads = runParameters["PlannedReads"]["Read"]
     # If runParameters["Reads"] is a single dict (common in single-read runs), 
     # this converts it to a list so the loop doesn't iterate over the keys.
-    if isinstance(reads, dict):
-        reads = [reads]
+    if isinstance(raw_reads, dict):
+        reads = [raw_reads]
+    else:
+        reads = raw_reads
 
     # Set values for LIMS UDFs
     process.udf["Run ID"] = runParameters["RunId"]
