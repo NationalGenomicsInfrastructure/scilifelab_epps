8e9b6f246046c40a098fe9704114019bcdf4c417
Anandashankar Anil <anandasanil@gmail.com>
2026-07-03 13:17:49 +0200

    Add expected clusters for 5B chemistry

--- scripts/manage_demux_stats_thresholds.py
+++ scripts/manage_demux_stats_thresholds.py
@@ -165,7 +165,9 @@ class Thresholds:
             elif self.chemistry == "S4":
                 self.exp_lane_clust = 2000e6
         elif self.instrument == "NovaSeqXPlus":
-            if self.chemistry == "10B":
+            if self.chemistry == "5B":
+                self.exp_lane_clust = 600e6
+            elif self.chemistry == "10B":
                 self.exp_lane_clust = 1200e6
             elif self.chemistry == "1.5B":
                 self.exp_lane_clust = 750e6
