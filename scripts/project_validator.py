d86c44e285c9b26242be3046a3a29d5556c656ff
Anandashankar Anil <anandasanil@gmail.com>
2026-07-17 16:32:00 +0200

    Add email notification on error, add granularity in error reeporting, do not include Ns in indexes

--- scripts/project_validator.py
+++ scripts/project_validator.py
@@ -1,9 +1,12 @@
 #!/usr/bin/env python
 
 import re
+import smtplib
 import sys
 import warnings
 from argparse import ArgumentParser
+from email.message import Message
+from email.mime.text import MIMEText
 from io import BytesIO
 
 import yaml
@@ -11,6 +14,9 @@ from genologics.config import BASEURI, PASSWORD, USERNAME
 from genologics.entities import Project
 from genologics.lims import Lims
 from openpyxl import load_workbook
+from openpyxl.worksheet.worksheet import Worksheet
+
+from scilifelab_epps.utils.get_epp_user import get_epp_user
 
 warnings.filterwarnings("ignore", category=UserWarning, module="openpyxl")
 
@@ -23,27 +29,65 @@ DESC = """EPP used to validate a project before it is released, including checki
 # Pre-compile regexes in global scope:
 NGISAMPLE_PAT = re.compile("P[0-9]+_[0-9]+")
 IDX_PAT = re.compile("([ATCG]{4,}N*)-?([ATCG]*)")
-VALIDBASES_PAT = re.compile(r"^[ATCGN\-]+$")
+VALIDBASES_PAT = re.compile(r"^[ATCG\-]+$")
 TENX_SINGLE_PAT = re.compile("SI-(?:GA|NA)-[A-H][1-9][0-2]?")
 TENX_DUAL_PAT = re.compile("SI-(?:TT|NT|NN|TN|TS)-[A-H][1-9][0-2]?")
 SMARTSEQ_PAT = re.compile("SMARTSEQ[1-9]?-[1-9][0-9]?[A-P]")
 
 
-def verify_samplename(sample_name, proj_id, file_name):
+def get_index_format_error(index: str) -> str | None:
+    if TENX_SINGLE_PAT.fullmatch(index):
+        return None
+    if TENX_DUAL_PAT.fullmatch(index):
+        return None
+    if SMARTSEQ_PAT.fullmatch(index):
+        return None
+
+    if not IDX_PAT.fullmatch(index):
+        return "does not match known index patterns"
+    parts = index.split("-")
+    if len(parts) > 2:
+        return "too many parts (expected single or dual index)"
+    if any(part == "" for part in parts):
+        return "empty part around '-'"
+
+    if not VALIDBASES_PAT.fullmatch(index):
+        return "contains invalid characters (allowed: A, T, C, G, -)"
+
+    return None
+
+
+def email_responsible(
+    message: str,
+    resp_email: str,
+    subject: str | None = None,
+) -> None:
+    msg: Message
+    body = "Samplesheet validation Errors: \n" + message
+    body += "\n\n--\nThis is an automatically generated error notification"
+    msg = MIMEText(body)
+    msg["Subject"] = subject
+
+    msg["From"] = "Lims_monitor"
+    msg["To"] = resp_email
+
+    with smtplib.SMTP("localhost") as s:
+        s.sendmail("genologics-lims@scilifelab.se", msg["To"], msg.as_string())
+
+
+def verify_samplename(sample_name: str, proj_id: str) -> set[str]:
     message = set()
     if not NGISAMPLE_PAT.findall(sample_name):
-        message.append(
-            f"[{file_name}] SAMPLE NAME WARNING: Bad sample name format {sample_name}"
-        )
+        message.add(f"SAMPLE NAME WARNING: Bad sample name format {sample_name}")
     else:
         if sample_name.split("_")[0] != proj_id:
-            message.append(
-                f"[{file_name}] SAMPLE NAME WARNING: Sample name {sample_name} does not match project ID {proj_id}"
+            message.add(
+                f"SAMPLE NAME WARNING: Sample name {sample_name} does not match project ID {proj_id}"
             )
     return message
 
 
-def my_distance(idx_a, idx_b):
+def my_distance(idx_a: str, idx_b: str) -> int:
     diffs = 0
     short = min((idx_a, idx_b), key=len)
     lon = idx_a if short == idx_b else idx_b
@@ -53,7 +97,9 @@ def my_distance(idx_a, idx_b):
     return diffs
 
 
-def parse_libary_info_sheet(worksheet, proj_id, file_name):
+def parse_library_info_sheet(
+    worksheet: Worksheet, proj_id: str
+) -> tuple[dict, set[str]]:
     data = {}
     message = set()
     for row in worksheet.iter_rows(
@@ -62,7 +108,7 @@ def parse_libary_info_sheet(worksheet, proj_id, file_name):
         sample_name = row[0]
         if sample_name is None:
             continue
-        message.update(verify_samplename(sample_name, proj_id, file_name))
+        message.update(verify_samplename(sample_name, proj_id))
         well = row[1]
         index = row[3]
         if well not in data:
@@ -78,35 +124,25 @@ def parse_libary_info_sheet(worksheet, proj_id, file_name):
                 )  # Remove the different length to avoid multiple warnings for the same issue
                 common_index = data[well]["index_length"].pop()
                 message.add(
-                    f"[{file_name}] INDEX LENGTH WARNING: Multiple index lengths noticed in pool {well} for Sample {sample_name}, length {len(index)} is different from {common_index}"
+                    f"INDEX LENGTH WARNING: Multiple index lengths noticed in pool {well} for Sample {sample_name}, length {len(index)} is different from {common_index}"
                 )
                 data[well]["index_length"].add(common_index)
 
         if index == "" or index is None:
             message.add(
-                f"[{file_name}] EMPTY INDEX: Sample {sample_name} in well {well} has an empty index"
+                f"EMPTY INDEX: Sample {sample_name} in well {well} has an empty index"
             )
         else:
             if index == "NoIndex":
                 if data[well]["count"] > 1:
                     message.add(
-                        f"[{file_name}] NOINDEX ERROR: Well {well} has NoIndex but but contains more than one sample"
+                        f"NOINDEX ERROR: Well {well} has NoIndex but but contains more than one sample"
                     )
             else:
-                if (
-                    (IDX_PAT.findall(index) and len(IDX_PAT.findall(index)) > 1)
-                    or (IDX_PAT.findall(index) and not VALIDBASES_PAT.findall(index))
-                    or (
-                        not (
-                            IDX_PAT.findall(index)
-                            or TENX_SINGLE_PAT.findall(index)
-                            or TENX_DUAL_PAT.findall(index)
-                            or SMARTSEQ_PAT.findall(index)
-                        )
-                    )
-                ):
-                    message.append(
-                        f"[{file_name}] INDEX FORMAT ERROR: Sample {sample_name} has a bad format or unknown index category\n"
+                reason = get_index_format_error(index)
+                if reason:
+                    message.add(
+                        f"INDEX FORMAT ERROR: Sample {sample_name} with index '{index}' has a bad format: {reason}"
                     )
                 else:
                     idxs = (
@@ -146,12 +182,12 @@ def parse_libary_info_sheet(worksheet, proj_id, file_name):
                                         + curr_idx.get("idx2", "")
                                     )
                                     if dist == 0:
-                                        message.append(
-                                            f"[{file_name}] INDEX COLLISION ERROR: Index {idx_a} and Index {idx_b} (for sample {sample_name}) in pool {well}"
+                                        message.add(
+                                            f"INDEX COLLISION ERROR: Index {idx_a} and Index {idx_b} (for sample {sample_name}) in pool {well}"
                                         )
                                     else:
-                                        message.append(
-                                            f"[{file_name}] SIMILAR INDEX WARNING: Index {idx_a} and Index {idx_b} (for sample {sample_name}) in pool {well}"
+                                        message.add(
+                                            f"SIMILAR INDEX WARNING: Index {idx_a} and Index {idx_b} (for sample {sample_name}) in pool {well}"
                                         )
                         except IndexError:
                             # try:
@@ -165,8 +201,8 @@ def parse_libary_info_sheet(worksheet, proj_id, file_name):
     return data, message
 
 
-def main(lims, pid, auto):
-    message = set()
+def main(lims: Lims, pid: str, auto: bool) -> None:
+    message = []
     project = Project(lims, id=pid)
     if not project.files:
         sys.stderr.write("No samplesheet file found for the project.\n")
@@ -184,14 +220,31 @@ def main(lims, pid, auto):
         # sample_information = 'Sample_information' in list(worksheet.iter_rows(min_row=4, max_row=4, values_only=True))[0][8]
 
         if library_information:
-            data, lib_info_message = parse_libary_info_sheet(worksheet, pid, file_name)
-            message.update(lib_info_message)
+            data, lib_info_message = parse_library_info_sheet(worksheet, pid)
+            if lib_info_message:
+                message.extend(
+                    [f"\n\nFile: {file_name} \n" + "\n".join(lib_info_message)]
+                )
     if message:
+        resp_email = get_epp_user(lims, project_id=pid).email
         if auto:
-            print(
-                "**Warnings from Verify samplesheet EPP: **\n" + "\n".join(message),
-                file=sys.stderr,
-            )
+            if not resp_email:
+                print(
+                    "**Errors exist in the samplesheet: **\n"
+                    "Email with the error could not be sent as no email address was found for the EPP user.\n"
+                    + "\n".join(message),
+                    file=sys.stderr,
+                )
+            else:
+                print(
+                    "**Errors exist in the samplesheet: **\n"
+                    f"Email with the error has been sent to {resp_email}.\n"
+                )
+                email_responsible(
+                    message="\n".join(message),
+                    resp_email=resp_email,
+                    subject=f"[Error] Project {pid} failed sample sheet validation",
+                )
         else:
             sys.stderr.write("; ".join(message))
     else:
